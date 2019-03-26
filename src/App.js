5ffec9d486a51c90c7db02698a91270114d29e0b
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-26 00:17:47 -0400

    adds starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <a href="">Nav Left</a>
+          <a href="">Nav Right</a>
         </header>
+        <main>
+          <article>MAIN CONTENT</article>
+          <nav>SIDE NAV</nav>
+          <aside>ASIDE</aside>
+        </main>
+        <footer>FOOTER</footer>
       </div>
     );
   }

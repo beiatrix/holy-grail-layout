6882df0d6bb3daba0cb62e090af82b09bfad2af0
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-28 21:25:06 -0400

    Updates

---
+++ precache-manifest.4108daf464e88b6c21bef7379a88ff69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80e56e01f63d29b66d3d",
+    "url": "/holy-grail-layout/static/js/runtime~main.d773e107.js"
+  },
+  {
+    "revision": "e303769b56e09bb3e70c",
+    "url": "/holy-grail-layout/static/js/main.ba37ec20.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/holy-grail-layout/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "e303769b56e09bb3e70c",
+    "url": "/holy-grail-layout/static/css/main.2fb18042.chunk.css"
+  },
+  {
+    "revision": "d018e0e75d8b8bf71029d9de33517f48",
+    "url": "/holy-grail-layout/index.html"
+  }
+];
